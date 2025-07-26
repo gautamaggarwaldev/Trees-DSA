577fa116c3d2fa4393090e57311e798ba6b86f01
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-07-26 22:24:36 +0530

    calculate no. of levels

---
+++ Binary Trees/5_NumberOfLevelsInBinaryTree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int levels(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    return 1 + max(levels(root->left), levels(root->right));
+}
+int main()
+{
+    Node *a = new Node(1); // root node
+    Node *b = new Node(2);
+    Node *c = new Node(3);
+    Node *d = new Node(4);
+    Node *e = new Node(5);
+    Node *f = new Node(6);
+    Node *g = new Node(7);
+
+    a->right = c;
+    a->left = b;
+    b->right = e;
+    b->left = d;
+    c->right = g;
+    c->left = f;
+
+    cout << "Number of Levels in Binary Tree: " << levels(a) << endl;
+
+    return 0;
+}
