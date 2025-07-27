3986a961e369d90a2bc0eb2cd4dd071cdc23c69e
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-07-27 23:50:07 +0530

    preorder traversal using stack

---
+++ Binary Trees/11_PreorderTraversalUsingIterativeMethod.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void printPreorder(Node *root)
+{
+    stack<Node *> s;
+    Node *temp = root;
+    while (temp != NULL || !s.empty())
+    {
+        if (temp != NULL)
+        {
+            cout << temp->val << " "; 
+            s.push(temp); 
+            temp = temp->left;
+        }
+        else
+        {
+            temp = s.top();
+            s.pop();
+            temp = temp->right;
+        }
+    }
+}
+int main()
+{
+    Node *a = new Node(1);
+    Node *b = new Node(2);
+    Node *c = new Node(3);
+    Node *d = new Node(4);
+    Node *e = new Node(5);
+    Node *f = new Node(6);
+    Node *g = new Node(7);
+
+    a->right = c;
+    a->left = b;
+    b->right = e;
+    b->left = d;
+    c->right = g;
+    c->left = f;
+
+    cout << "Inorder Traversal: ";
+    printPreorder(a);
+}
