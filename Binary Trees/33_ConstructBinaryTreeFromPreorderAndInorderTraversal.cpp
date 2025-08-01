5a8eb1e05e7272eb408fc983a5a2d73f3f5d1e20
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-08-01 13:16:45 +0530

    Construct Binary Tree from Preorder and Inorder Traversal

--- Binary Trees/33_ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
+++ Binary Trees/33_ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class TreeNode
@@ -15,18 +16,14 @@ public:
 class Solution
 {
 public:
-    int findPos(vector<int> &in, int ele, int n)
+    void mapping(vector<int> &in, map<int, int> &mp, int n) // Function to map inorder elements with their indices
     {
         for (int i = 0; i < n; i++)
         {
-            if (in[i] == ele)
-            {
-                return i;
-            }
+            mp[in[i]] = i;
         }
-        return -1;
     }
-    TreeNode *solve(vector<int> &pre, vector<int> &in, int inStartIdx, int inEndIdx, int &preIdx, int n)
+    TreeNode *solve(vector<int> &pre, vector<int> &in, int inStartIdx, int inEndIdx, int &preIdx, int n, map<int, int> &mp)
     {
         if (preIdx >= n || inStartIdx > inEndIdx)
         {
@@ -34,17 +31,19 @@ public:
         }
         int ele = pre[preIdx++]; // Get the current element from preorder
         TreeNode *root = new TreeNode(ele); // Create a new TreeNode with the current element
-        int pos = findPos(in, ele, n); // Find the position of the current element in inorder traversal
+        int pos = mp[ele]; // Find the index of the current element in inorder using the map
 
-        root->left = solve(pre, in, inStartIdx, pos - 1, preIdx, n); // Recursively build the left subtree
-        root->right = solve(pre, in, pos + 1, inEndIdx, preIdx, n); // Recursively build the right subtree
+        root->left = solve(pre, in, inStartIdx, pos - 1, preIdx, n, mp);
+        root->right = solve(pre, in, pos + 1, inEndIdx, preIdx, n, mp);
         return root;
     }
     TreeNode *buildTree(vector<int> &pre, vector<int> &in)
     {
         int n = pre.size();
+        map<int, int> mp; // map inorder element with their index
+        mapping(in, mp, n);
         int preIdx = 0; // Index for preorder traversal
-        TreeNode *ans = solve(pre, in, 0, n - 1, preIdx, n);
+        TreeNode *ans = solve(pre, in, 0, n - 1, preIdx, n, mp);
         return ans;
     }
 };
@@ -54,6 +53,6 @@ int main()
     vector<int> preorder = {3, 9, 20, 15, 7};
     vector<int> inorder = {9, 3, 15, 20, 7};
     TreeNode *root = sol.buildTree(preorder, inorder);
-    
+
     return 0;
 }
